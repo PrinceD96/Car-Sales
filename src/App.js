886846682c2c322a3bc223844d677d61244b3416
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-04 16:11:32 -0500

    Stretch done! Code refactored from HOCs to Redux hooks.

--- src/App.js
+++ src/App.js
@@ -4,40 +4,42 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
+// import { connect } from "react-redux";
 
-import { connect } from "react-redux";
+//For Stretch Goals I will refactor from HOCs to Redux hooks
+// Step 1 Import useSelector hook
+import { useSelector } from "react-redux";
+// Step 2 Delete all props
 
-// console.log("store", store);
-
-const App = props => {
-	const removeFeature = item => {
-		// dispatch an action here to remove an item
-	};
-
-	const buyItem = item => {
-		// dipsatch an action here to add an item
-	};
+const App = () => {
+	// Step 3 Implement useSelector to get the pieces of state needed from Redux store
+	const additionalPrice = useSelector(state => state.additionalPrice);
+	const car = useSelector(state => state.car);
+	const additionalFeatures = useSelector(state => state.additionalFeatures);
 
 	return (
 		<div className='boxes'>
 			<div className='box'>
-				<Header car={props.car} />
-				<AddedFeatures car={props.car} />
+				<Header car={car} />
+				<AddedFeatures car={car} />
 			</div>
 			<div className='box'>
-				<AdditionalFeatures additionalFeatures={props.additionalFeatures} />
-				<Total car={props.car} additionalPrice={props.additionalPrice} />
+				<AdditionalFeatures additionalFeatures={additionalFeatures} />
+				<Total car={car} additionalPrice={additionalPrice} />
 			</div>
 		</div>
 	);
 };
 
-const mapStateToProps = state => {
-	return {
-		additionalPrice: state.additionalPrice,
-		car: state.car,
-		additionalFeatures: state.additionalFeatures
-	};
-};
+//Step 4 Change the export to not use connect anymore
+export default App;
 
-export default connect(mapStateToProps, {})(App);
+// const mapStateToProps = state => {
+// 	return {
+// 		additionalPrice: state.additionalPrice,
+// 		car: state.car,
+// 		additionalFeatures: state.additionalFeatures
+// 	};
+// };
+
+// export default connect(mapStateToProps, {})(App);

124366d04b231f94d48d5e1a92b07b076b9e8507
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-04 15:47:01 -0500

    MVP met! TOTAL updates as features are added and removed.

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -29,10 +29,11 @@ export const appReducer = (state = initialState, action) => {
 		case REMOVE_FEATURE:
 			return {
 				...state,
+				additionalPrice: state.additionalPrice - action.payload.price,
 				car: {
 					...state.car,
 					features: state.car.features.filter(
-						feature => feature.id !== action.payload
+						feature => feature.id !== action.payload.id
 					)
 				}
 			};
